cfeaebfd224bb35a82e311af141feac062e82614
Manfred Touron <m@42.am>
2016-04-27 12:47:40 +0200

    Bump assh@2.3.0

--- assh.rb
+++ assh.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Assh < Formula
   desc "assh: Advanced SSH config - A transparent wrapper that adds regex, aliases, gateways, includes, dynamic hostnames to SSH"
   homepage "https://github.com/moul/advanced-ssh-config"
-  url "https://github.com/moul/advanced-ssh-config/archive/v2.2.0.tar.gz"
-  sha256 "8a71969df06e8db0bd4b06cf928780e45e22cf5f131f06aec43cccd955ee2ead"
+  url "https://github.com/moul/advanced-ssh-config/archive/v2.3.0.tar.gz"
+  sha256 "d2903d3723c8349ec09bc8c7ada1fcb60d835f248d4df1faf5fe6fbadf484f16"
 
   head "https://github.com/moul/advanced-ssh-config.git"
 
@@ -20,6 +20,14 @@ class Assh < Formula
 
     bash_completion.install "contrib/completion/bash_autocomplete"
     zsh_completion.install "contrib/completion/zsh_autocomplete"
+  end
+
+  def caveats
+    <<-EOS.undent
+    To activate advanced pattern matching, add the following at the end of your .bashrc or .zshrc:
+
+      alias ssh="assh wrapper ssh"
+    EOS
   end
 
   test do
