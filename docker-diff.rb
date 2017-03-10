e34c178633c9598258096167766ad09032dc3f55
Manfred Touron <m@42.am>
2017-03-10 22:10:42 +0100

    Bump docker-diff@1.1

--- docker-diff.rb
+++ docker-diff.rb
@@ -1,8 +1,8 @@
 class DockerDiff < Formula
   desc "Compare Docker images"
   homepage "https://github.com/moul/docker-diff"
-  url "https://github.com/moul/docker-diff/archive/v1.0.tar.gz"
-  sha256 "21826141f9161f66a14f38c617a7f9c459d34439c47daf7029ea7a8ec83dc8c2"
+  url "https://github.com/moul/docker-diff/archive/v1.1.tar.gz"
+  sha256 "583f0649df411c107e0dac52d7bfeb191d9579f3d490ecb1c9d0022ba7db1f5f"
   head "https://github.com/moul/docker-diff.git"
   def install
     bin.install "docker-diff"
